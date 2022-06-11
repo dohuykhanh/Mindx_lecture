9c6051e0f3a4497d571f0144ccf96a6aac3816ad
Zippochoiloto <theanhptit94@gmail.com>
2022-06-11 23:41:44 +0700

    nodejs session 5

---
+++ nodejs_session_5/index.js
@@ -0,0 +1,23 @@
+const {MongoClient} = require('mongodb')
+
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+const dbName = 'School_Management';
+
+async function main() {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('Class');
+  
+    // the following code examples can be pasted here...
+  
+    return 'done.';
+  }
+  
+  main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
